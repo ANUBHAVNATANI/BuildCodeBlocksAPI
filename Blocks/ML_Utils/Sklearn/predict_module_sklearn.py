33ca14c30e7bf612efae55772518c377be7735aa
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-05-28 18:15:34 +0530

    basic_block_struct add

---
+++ Blocks/ML_Utils/Sklearn/predict_module_sklearn.py
@@ -0,0 +1,9 @@
+# Predict Function
+"""
+Sklearn module prediction function
+Docs--
+"""
+
+
+def predict(X, model):
+    return model.predict(X)
---
+++ Blocks/ML_Utils/Sklearn/train_module_sklearn.py
@@ -0,0 +1,10 @@
+# Train Function
+"""
+Sklearn module training function
+Docs--
+"""
+
+
+def train(X, y, model):
+    model.fit(X, y)
+    return model
