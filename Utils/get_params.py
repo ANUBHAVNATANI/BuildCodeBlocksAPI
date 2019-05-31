d5a41ceef1d9ee6e576303c6ccee62cb6f99df20
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-05-31 09:49:55 +0530

    cleaned repo

--- Utils/get_params.py
+++
@@ -1,9 +0,0 @@
-"""
-This file get the params and set it for the model and then use all the other properties to make the python file compile
-"""
-import requests
-resp = requests.get("URL")
-if resp.status_code != 200:
-    print("Error")
-for things in resp.json():
-    print('{} {}'.format(things['modelName'], things['params']))
